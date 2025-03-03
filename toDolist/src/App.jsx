3fcb12bc7bfec2dd904cfbf44a3213bdedaa13ad
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2025-03-03 11:10:24 +0100

    project insitialisation

---
+++ toDolist/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import React from 'react'
+import HelloWord from './components/HelloWorld'
+
+const App = () =>{
+  return(
+    <div>
+      <HelloWord />
+    </div>
+  )
+};
+export default App; 
---
+++ toDolist/src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const name = 'React';
+ const HelloWord =()=>{
+    return(
+        <h1> Hello, {name} !</h1>
+    );
+ }; 
+ export default HelloWord ;
