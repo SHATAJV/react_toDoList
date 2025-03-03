f6fe9672fe1806e0461a3b4361c173a005e0b286
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2025-03-03 15:37:57 +0100

    axios

--- toDolist/src/App.jsx
+++ toDolist/src/App.jsx
@@ -1,36 +1,50 @@
 
 import './App.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from './components/Task';
 import ToDolist from './components/ToDoList';
 import AddTodoform from './components/AddTodoForm';
 
 const App = () =>{
-  const [tasks,setTasks] = useState([
-    {id: 1, title :"learn React"},
-    {id:2, title: "create a toDoList"},
-    {id:3, title: "practice more"},
-    {id:4, title: "Do Sport"}
-
-  ]);
+  const [tasks,setTasks] = useState([]);
+  useEffect(()=>{
+    axios
+        .get("http://localhost:5000/tasks")
+        .then((response)=> setTasks(response.data))
+        .catch((error) => console.error("error to recieve the tasks:", error));
+  }, []
+  );
 
   const handleAddTask = (title) =>{
-    const newTask = {id: tasks.length +1, title};
-    setTasks([...tasks, newTask])
+    const newTask = { title, completed: false};
+    axios
+        .post("http://localhost:5000/tasks")
+        .then((response)=> setTasks([...tasks, response.data]))
+        .catch((error) => console.error("error to add the tasks:", error));
+    
   };
-  const toggleTask = (taskId) => {
-    setTasks(tasks.map(task =>
-      task.id === taskId ? {...task,completed: !task.completed} : task
-    ));
-  };
-  return(
-    <div>
-      <h1> ToDo List with React</h1>
-      <AddTodoform onAddTask={handleAddTask} />
-      <ToDolist tasks={tasks} onToggle={toggleTask}/>
-    </div>
-  );
-  
-  
+  const handdleToggleTask = (id) =>{
+    const task = task.find((task)=>task.id===id);
+    const updateTask = {...task, completed: !task.completed};
+    axios
+        .put (`http://localhost:5000/tasks/${id}`,updateTask)
+        .then(()=> setTasks(tasks.map((t)=> (t.id===id ? updateTask :t))))
+        .catch((error) => console.error("error to update the tasks:", error));
+  };  
+
+        const toggleTask = (taskId) => {
+          setTasks(tasks.map(task =>
+            task.id === taskId ? {...task,completed: !task.completed} : task
+          ));
+        };
+        return(
+          <div>
+            <h1> ToDo List with React</h1>
+            <AddTodoform onAddTask={handleAddTask} />
+            <ToDolist tasks={tasks} onToggle={toggleTask}/>
+          </div>
+        );
+        
+    
 };
 export default App; 
